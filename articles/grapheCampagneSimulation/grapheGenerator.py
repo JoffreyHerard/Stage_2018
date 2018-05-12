e5847b7b0768b5b999ec099b8a0ca4391b843c47
Apocalypzer <apocalypzer@gmail.com>
2018-05-12 15:32:20 +0200

    modifying ay the graph is generated

--- articles/grapheCampagneSimulation/grapheGenerator.py
+++ articles/grapheCampagneSimulation/grapheGenerator.py
@@ -3,7 +3,9 @@
 from igraph import *
 import sys
 import random
+import os
 
+file_written=0
 def whatsthat(number, well, gw, IN):
 	if number not in well:
 		if number not in gw:
@@ -15,15 +17,18 @@ def whatsthat(number, well, gw, IN):
 	else:
 		toR=("WELL",well.index(number))
 		return toR
-def createGraph(lower_bound,upper_bound,filename):
+def createGraph(lower_bound,upper_bound,filename,max_degree_IN,max_degree_GW,nb_vertex_max_input):
+	global file_written
+	#print("\t\t   -----SIMULATION AVEC PARAMETRES SUIVANT :-----")
+	#print("\t lower_bound : "+str(lower_bound)+"/ upper_bound : "+str(upper_bound)+" /nom de fichier :"+filename+"/ max degree IN "+str(max_degree_IN)+"/max_degree_GW "+str(max_degree_GW))
 	#Random number is the probability which have a LoraGateway to have one isolated node
 	p=random.random()
 	#Random number is the probability which have a LoraGateway to have another isolated node
 	p2=random.random()
 	current_graph=0
-	nb_max_vertex=1000
-	nb_degree_max_IN=4
-	nb_degree_max_GW=4
+	nb_max_vertex=nb_vertex_max_input
+	nb_degree_max_IN=max_degree_IN
+	nb_degree_max_GW=max_degree_GW
 	names=[]
 	id_well=[]
 	id_gw=[]
@@ -46,6 +51,7 @@ def createGraph(lower_bound,upper_bound,filename):
 		#on repertorie l'id du puit
 		id_well.append(nb_vertex)
 		names.append("WELL")
+		#names.append(str(nb_vertex))
 		current_well=nb_vertex
 		#on augmente le nombre de sommet actuel
 		nb_vertex=nb_vertex+1
@@ -55,11 +61,14 @@ def createGraph(lower_bound,upper_bound,filename):
 		nb_loop_GW=0
 		while nb_vertex <nb_max_vertex and actual_degree_GW < nb_degree_max_GW and nb_loop_GW < nb_degree_max_GW:
 			# x <- tirer probabilite
+			#print("PREMIER WHILE : "+ str(nb_vertex) +" < "+ str(nb_max_vertex) +" ET "+str(actual_degree_GW)+" < " + str(nb_degree_max_GW) +" AND "+str(nb_loop_GW)+" < "+str(nb_degree_max_GW))
 			current_gw=0
 		 	x=random.random()
 			#Si x > p1
-			if x > p :
+			if x > p and actual_degree_GW < nb_degree_max_GW:
+
 				names.append("GW")
+				#names.append(str(nb_vertex))
 				nbgw=nbgw+1
 				actual_degree_GW=actual_degree_GW+1
 				#on repertorie l'id de la gw
@@ -73,7 +82,26 @@ def createGraph(lower_bound,upper_bound,filename):
 				#Tant que nb_noeud < 1000 ET degre_actuel < degre max ET iteration< degre max faire
 				actual_degree_IN=0
 				nb_loop_IN=0
+
+				#au moins un IN
+
+				nbin=nbin+1
+				names.append("IN")
+				#names.append(str(nb_vertex))
+				actual_degree_IN=actual_degree_IN+1
+				#on repertorie l'id de l'IN
+				id_in.append(nb_vertex)
+				#rajouter IN
+				g.add_vertices(1)
+				current_in=nb_vertex
+				nb_vertex=nb_vertex+1
+				#cree connection entre GW et IN
+				g.add_edges([(current_gw,current_in)])
+				#print(" On ajoute un IN entre"+str(current_gw)+" et "+str(current_in))
+
 				while nb_vertex <nb_max_vertex and actual_degree_IN < nb_degree_max_IN and nb_loop_IN < nb_degree_max_IN:
+					#print("APRES DEUXIEME WHILE : "+ str(nb_vertex) +" < "+ str(nb_max_vertex) +" ET "+str(actual_degree_IN)+" < " + str(nb_degree_max_IN) +" AND "+str(nb_loop_IN)+" < "+str(nb_degree_max_IN))
+
 					# y <- tirer probabilite
 					current_in=0
 				 	y=random.random()
@@ -81,6 +109,7 @@ def createGraph(lower_bound,upper_bound,filename):
 					if y > p2 :
 						nbin=nbin+1
 						names.append("IN")
+						#names.append(str(nb_vertex))
 						actual_degree_IN=actual_degree_IN+1
 						#on repertorie l'id de l'IN
 						id_in.append(nb_vertex)
@@ -90,12 +119,15 @@ def createGraph(lower_bound,upper_bound,filename):
 						nb_vertex=nb_vertex+1
 						#cree connection entre GW et IBN
 						g.add_edges([(current_gw,current_in)])
+						#print(" On ajoute un IN entre"+str(current_gw)+" et "+str(current_in))
 					nb_loop_IN=nb_loop_IN+1
 			nb_loop_GW=nb_loop_GW+1
 	g.vs["label"]=names
 	g.es[0]
 	#g.write_dot(filename+".dot")
 	#Making file .ned
+	file_written=file_written+1
+	#print "graphe/"+filename+".ned"
 	fic=open("graphe/"+filename+".ned","w")
 	fic.write("package lora.simulations;\n")
 	fic.write("import lora.*;\n")
@@ -122,38 +154,330 @@ def createGraph(lower_bound,upper_bound,filename):
 
 	fic.write("}")
 	fic.close()
+
+
+
+
+
+
+
+#DEBUT 10 MESSAGES PAR JOUR
+	# AGREGATION
 	#Making file omnetpp.ini
-	fic=open("omnetppini/"+filename+".ini","w")
+	#print "omnetppini/"+filename+"-10MSG.ini"
+	file_written=file_written+1
+	fic=open("omnetppini/"+filename+"-10MSG.ini","w")
 	fic.write("[General]\n")
+	fic.write("user-interface=Cmdenv\n")
 	fic.write("network = lora.simulations.Net\n")
-	fic.write("sim-time-limit=1000s\n")
+	fic.write("sim-time-limit=86400s\n")
+	fic.write("repeat = 10\n")
+	fic.write("ned-path=/Users/joffreyherard/Documents/Master_2/Projet_fin_etude/dev/src/src;/Users/joffreyherard/Documents/Master_2/Projet_fin_etude/dev/src/simulations\n")
 	i = 0
 	while i < nbwell:
 		fic.write("Net.WELL["+str(i)+"].id="+str(i)+"\n")
+		fic.write("Net.WELL["+str(i)+"].slot="+str(8640)+"\n") #changer pour nb message
 		i=i+1
 	j = 0
 	while j < nbgw:
 		fic.write("Net.WGW["+str(j)+"].id="+str(i)+"\n")
+		fic.write("Net.WGW["+str(j)+"].file=\""+filename+"-10MSG\"\n")
+		fic.write("Net.WGW["+str(j)+"].agreg=true\n")#changer pour false
+		fic.write("Net.WGW["+str(j)+"].battery="+str(23760000)+"\n")
 		i=i+1
 		j=j+1
 	k = 0
 	while k < nbin:
 		fic.write("Net.IN["+str(k)+"].id="+str(i)+"\n")
+		fic.write("Net.IN["+str(k)+"].file=\""+filename+"-10MSG\"\n")
+		fic.write("Net.IN["+str(k)+"].battery="+str(23760000)+"\n")
 		k=k+1
 		i=i+1
 	fic.close()
 
-if(len(sys.argv) ==3):
+	file_written=file_written+1
+	#print "omnetppini/"+filename+"-NA-10MSG.ini"
+	fic=open("omnetppini/"+filename+"-NA-10MSG.ini","w")
+	fic.write("[General]\n")
+	fic.write("user-interface=Cmdenv\n")
+	fic.write("network = lora.simulations.Net\n")
+	fic.write("sim-time-limit=86400s\n")
+	fic.write("repeat = 10\n")
+	fic.write("ned-path=/Users/joffreyherard/Documents/Master_2/Projet_fin_etude/dev/src/src;/Users/joffreyherard/Documents/Master_2/Projet_fin_etude/dev/src/simulations\n")
+	i = 0
+	while i < nbwell:
+		fic.write("Net.WELL["+str(i)+"].id="+str(i)+"\n")
+		fic.write("Net.WELL["+str(i)+"].slot="+str(8640)+"\n") #changer pour nb message
+		i=i+1
+	j = 0
+	while j < nbgw:
+		fic.write("Net.WGW["+str(j)+"].id="+str(i)+"\n")
+		fic.write("Net.WGW["+str(j)+"].file=\""+filename+"-NA-10MSG\"\n")
+		fic.write("Net.WGW["+str(j)+"].agreg=false\n")#changer pour false
+		fic.write("Net.WGW["+str(j)+"].battery="+str(23760000)+"\n")
+		i=i+1
+		j=j+1
+	k = 0
+	while k < nbin:
+		fic.write("Net.IN["+str(k)+"].id="+str(i)+"\n")
+		fic.write("Net.IN["+str(k)+"].file=\""+filename+"-NA-10MSG\"\n")
+		fic.write("Net.IN["+str(k)+"].battery="+str(23760000)+"\n")
+		k=k+1
+		i=i+1
+	fic.close()
+
+
+
+#FIN 10 MESSAGES PAR JOUR
+
+
+
+
+#DEBUT 1 MESSAGES PAR JOUR 12 h
+
+	# AGREGATION
+	#Making file omnetpp.ini
+	file_written=file_written+1
+	#print "omnetppini/"+filename+"-1MSG.ini"
+	fic=open("omnetppini/"+filename+"-1MSG.ini","w")
+	fic.write("[General]\n")
+	fic.write("user-interface=Cmdenv\n")
+	fic.write("network = lora.simulations.Net\n")
+	fic.write("sim-time-limit=86400s\n")
+	fic.write("repeat = 10\n")
+	fic.write("ned-path=/Users/joffreyherard/Documents/Master_2/Projet_fin_etude/dev/src/src;/Users/joffreyherard/Documents/Master_2/Projet_fin_etude/dev/src/simulations\n")
+	i = 0
+	while i < nbwell:
+		fic.write("Net.WELL["+str(i)+"].id="+str(i)+"\n")
+		fic.write("Net.WELL["+str(i)+"].slot="+str(86400)+"\n") #changer pour nb message
+		fic.write("Net.WELL["+str(i)+"].file=\""+filename+"-1MSG\"\n")
+		i=i+1
+	j = 0
+	while j < nbgw:
+		fic.write("Net.WGW["+str(j)+"].id="+str(i)+"\n")
+		fic.write("Net.WGW["+str(j)+"].file=\""+filename+"-1MSG\"\n")
+		fic.write("Net.WGW["+str(j)+"].agreg=true\n")#changer pour false
+		fic.write("Net.WGW["+str(j)+"].battery="+str(23760000)+"\n")
+		i=i+1
+		j=j+1
+	k = 0
+	while k < nbin:
+		fic.write("Net.IN["+str(k)+"].id="+str(i)+"\n")
+		fic.write("Net.IN["+str(k)+"].file=\""+filename+"-1MSG\"\n")
+		fic.write("Net.IN["+str(k)+"].battery="+str(23760000)+"\n")
+		k=k+1
+		i=i+1
+	fic.close()
+
+
+	file_written=file_written+1
+	#print "omnetppini/"+filename+"-NA-1MSG.ini"
+	fic=open("omnetppini/"+filename+"-NA-1MSG.ini","w")
+	fic.write("[General]\n")
+	fic.write("user-interface=Cmdenv\n")
+	fic.write("network = lora.simulations.Net\n")
+	fic.write("sim-time-limit=86400s\n")
+	fic.write("repeat = 10\n")
+	fic.write("ned-path=/Users/joffreyherard/Documents/Master_2/Projet_fin_etude/dev/src/src;/Users/joffreyherard/Documents/Master_2/Projet_fin_etude/dev/src/simulations\n")
+	i = 0
+	while i < nbwell:
+		fic.write("Net.WELL["+str(i)+"].id="+str(i)+"\n")
+		fic.write("Net.WELL["+str(i)+"].slot="+str(86400)+"\n") #changer pour nb message
+		fic.write("Net.WELL["+str(i)+"].file=\""+filename+"-NA-1MSG\"\n")
+		i=i+1
+	j = 0
+	while j < nbgw:
+		fic.write("Net.WGW["+str(j)+"].id="+str(i)+"\n")
+		fic.write("Net.WGW["+str(j)+"].file=\""+filename+"-NA-1MSG\"\n")
+		fic.write("Net.WGW["+str(j)+"].agreg=true\n")#changer pour false
+		fic.write("Net.WGW["+str(j)+"].battery="+str(23760000)+"\n")
+		i=i+1
+		j=j+1
+	k = 0
+	while k < nbin:
+		fic.write("Net.IN["+str(k)+"].id="+str(i)+"\n")
+		fic.write("Net.IN["+str(k)+"].file=\""+filename+"-NA-1MSG\"\n")
+		fic.write("Net.IN["+str(k)+"].battery="+str(23760000)+"\n")
+		k=k+1
+		i=i+1
+	fic.close()
+
+
+
+#FIN 1 MESSAGES PAR JOUR
+
+
+
+#DEBUT 2 MESSAGES PAR JOUR 12 h
+
+	# AGREGATION
+	#Making file omnetpp.ini
+	file_written=file_written+1
+	#print "omnetppini/"+filename+"-2MSG.ini"
+	fic=open("omnetppini/"+filename+"-2MSG.ini","w")
+	fic.write("[General]\n")
+	fic.write("user-interface=Cmdenv\n")
+	fic.write("network = lora.simulations.Net\n")
+	fic.write("sim-time-limit=86400s\n")
+	fic.write("repeat = 10\n")
+	fic.write("ned-path=/Users/joffreyherard/Documents/Master_2/Projet_fin_etude/dev/src/src;/Users/joffreyherard/Documents/Master_2/Projet_fin_etude/dev/src/simulations\n")
+	i = 0
+	while i < nbwell:
+		fic.write("Net.WELL["+str(i)+"].id="+str(i)+"\n")
+		fic.write("Net.WELL["+str(i)+"].slot="+str(43200)+"\n") #changer pour nb message
+		fic.write("Net.WELL["+str(i)+"].file=\""+filename+"-2MSG\"\n")
+		i=i+1
+	j = 0
+	while j < nbgw:
+		fic.write("Net.WGW["+str(j)+"].id="+str(i)+"\n")
+		fic.write("Net.WGW["+str(j)+"].file=\""+filename+"-2MSG\"\n")
+		fic.write("Net.WGW["+str(j)+"].agreg=true\n")#changer pour false
+		fic.write("Net.WGW["+str(j)+"].battery="+str(23760000)+"\n")
+		i=i+1
+		j=j+1
+	k = 0
+	while k < nbin:
+		fic.write("Net.IN["+str(k)+"].id="+str(i)+"\n")
+		fic.write("Net.IN["+str(k)+"].file=\""+filename+"-2MSG\"\n")
+		fic.write("Net.IN["+str(k)+"].battery="+str(23760000)+"\n")
+		k=k+1
+		i=i+1
+	fic.close()
+
+	file_written=file_written+1
+	#print "omnetppini/"+filename+"-NA-2MSG.ini"
+	fic=open("omnetppini/"+filename+"-NA-2MSG.ini","w")
+	fic.write("[General]\n")
+	fic.write("user-interface=Cmdenv\n")
+	fic.write("network = lora.simulations.Net\n")
+	fic.write("sim-time-limit=86400s\n")
+	fic.write("repeat = 10\n")
+	fic.write("ned-path=/Users/joffreyherard/Documents/Master_2/Projet_fin_etude/dev/src/src;/Users/joffreyherard/Documents/Master_2/Projet_fin_etude/dev/src/simulations\n")
+	i = 0
+	while i < nbwell:
+		fic.write("Net.WELL["+str(i)+"].id="+str(i)+"\n")
+		fic.write("Net.WELL["+str(i)+"].slot="+str(43200)+"\n") #changer pour nb message
+		fic.write("Net.WELL["+str(i)+"].file=\""+filename+"-NA-2MSG\"\n")
+		i=i+1
+	j = 0
+	while j < nbgw:
+		fic.write("Net.WGW["+str(j)+"].id="+str(i)+"\n")
+		fic.write("Net.WGW["+str(j)+"].file=\""+filename+"-NA-2MSG\"\n")
+		fic.write("Net.WGW["+str(j)+"].agreg=true\n")#changer pour false
+		fic.write("Net.WGW["+str(j)+"].battery="+str(23760000)+"\n")
+		i=i+1
+		j=j+1
+	k = 0
+	while k < nbin:
+		fic.write("Net.IN["+str(k)+"].id="+str(i)+"\n")
+		fic.write("Net.IN["+str(k)+"].file=\""+filename+"-NA-2MSG\"\n")
+		fic.write("Net.IN["+str(k)+"].battery="+str(23760000)+"\n")
+		k=k+1
+		i=i+1
+	fic.close()
+
+
+
+#FIN 2 MESSAGES PAR JOUR
+
+
+
+
+#DEBUT 1 MESSAGES PAR HEURE
+
+	# AGREGATION
+	#Making file omnetpp.ini
+	file_written=file_written+1
+	#print "omnetppini/"+filename+"-24MSG.ini"
+	fic=open("omnetppini/"+filename+"-24MSG.ini","w")
+	fic.write("[General]\n")
+	fic.write("user-interface=Cmdenv\n")
+	fic.write("network = lora.simulations.Net\n")
+	fic.write("sim-time-limit=86400s\n")
+	fic.write("repeat = 10\n")
+	fic.write("ned-path=/Users/joffreyherard/Documents/Master_2/Projet_fin_etude/dev/src/src;/Users/joffreyherard/Documents/Master_2/Projet_fin_etude/dev/src/simulations\n")
+	i = 0
+	while i < nbwell:
+		fic.write("Net.WELL["+str(i)+"].id="+str(i)+"\n")
+		fic.write("Net.WELL["+str(i)+"].slot="+str(3600)+"\n") #changer pour nb message
+		fic.write("Net.WELL["+str(i)+"].file=\""+filename+"-24MSG\"\n")
+		i=i+1
+	j = 0
+	while j < nbgw:
+		fic.write("Net.WGW["+str(j)+"].id="+str(i)+"\n")
+		fic.write("Net.WGW["+str(j)+"].file=\""+filename+"-24MSG\"\n")
+		fic.write("Net.WGW["+str(j)+"].agreg=true\n")#changer pour false
+		fic.write("Net.WGW["+str(j)+"].battery="+str(23760000)+"\n")
+		i=i+1
+		j=j+1
+	k = 0
+	while k < nbin:
+		fic.write("Net.IN["+str(k)+"].id="+str(i)+"\n")
+		fic.write("Net.IN["+str(k)+"].file=\""+filename+"-24MSG\"\n")
+		fic.write("Net.IN["+str(k)+"].battery="+str(23760000)+"\n")
+		k=k+1
+		i=i+1
+	fic.close()
+
+	file_written=file_written+1
+	#print "omnetppini/"+filename+"-NA-24MSG.ini"
+	fic=open("omnetppini/"+filename+"-NA-24MSG.ini","w")
+	fic.write("[General]\n")
+	fic.write("user-interface=Cmdenv\n")
+	fic.write("network = lora.simulations.Net\n")
+	fic.write("sim-time-limit=86400s\n")
+	fic.write("repeat = 10\n")
+	fic.write("ned-path=/Users/joffreyherard/Documents/Master_2/Projet_fin_etude/dev/src/src;/Users/joffreyherard/Documents/Master_2/Projet_fin_etude/dev/src/simulations\n")
+	i = 0
+	while i < nbwell:
+		fic.write("Net.WELL["+str(i)+"].id="+str(i)+"\n")
+		fic.write("Net.WELL["+str(i)+"].slot="+str(3600)+"\n") #changer pour nb message
+		fic.write("Net.WELL["+str(i)+"].file=\""+filename+"-NA-24MSG\"\n")
+		i=i+1
+	j = 0
+	while j < nbgw:
+		fic.write("Net.WGW["+str(j)+"].id="+str(i)+"\n")
+		fic.write("Net.WGW["+str(j)+"].file=\""+filename+"-NA-24MSG\"\n")
+		fic.write("Net.WGW["+str(j)+"].agreg=true\n")#changer pour false
+		fic.write("Net.WGW["+str(j)+"].battery="+str(23760000)+"\n")
+		i=i+1
+		j=j+1
+	k = 0
+	while k < nbin:
+		fic.write("Net.IN["+str(k)+"].id="+str(i)+"\n")
+		fic.write("Net.IN["+str(k)+"].file=\""+filename+"-NA-24MSG\"\n")
+		fic.write("Net.IN["+str(k)+"].battery="+str(23760000)+"\n")
+		k=k+1
+		i=i+1
+	fic.close()
+
+
+#FIN 1 MESSAGES PAR HEURE
+
+
+
+
+if(len(sys.argv) ==6):
 	#Getting numbers of LoRaWAN gateways and LoRagateways and Isolated nodes
 	lower_bound= int(sys.argv[1])
 	upper_bound= int(sys.argv[2])
+	max_degree_IN= int(sys.argv[3])
+	max_degree_GW= int(sys.argv[4])
+	nb_vertex= int(sys.argv[5])
 	nb_graph=100
-	i = 0
-	while i < nb_graph:
-		filename="graphe"+str(i)
-		createGraph(lower_bound,upper_bound,filename)
-		i=i+1
-	print("JOB FINISHED")
+	current_degree_IN=1
+	current_degree_GW=1
+	while current_degree_IN < max_degree_IN+1:
+		while current_degree_GW < max_degree_GW+1:
+			i = 0
+			while i < nb_graph:
+				filename="graphe"+str(i)+"_IN_"+str(current_degree_IN)+"_GW_"+str(current_degree_GW)
+				createGraph(lower_bound,upper_bound,filename,current_degree_IN,current_degree_GW,nb_vertex)
+				i=i+1
+			current_degree_GW=current_degree_GW+1
+		current_degree_GW=1
+		current_degree_IN=current_degree_IN+1
+	print(str(file_written)+"files written")
 else:
 	print "3 arguments is needed, the right way to use this python script is :\n"
-	print "python grapheGenerator.py <LOWER BOUND> <UPPER BOUND>"
+	print "python grapheGenerator.py <LOWER BOUND> <UPPER BOUND> <MAX IN PER GW> <MAX GW PER WELL> <MAX VERTEX>"
---
+++ articles/grapheCampagneSimulation/makeTest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+dot -Tjpeg graphe0_IN_1_GW_1.dot  -o graphe0_IN_1_GW_1.jpg
+dot -Tjpeg graphe0_IN_1_GW_2.dot  -o graphe0_IN_1_GW_2.jpg
+dot -Tjpeg graphe0_IN_1_GW_3.dot  -o graphe0_IN_1_GW_3.jpg
+dot -Tjpeg graphe0_IN_1_GW_4.dot  -o graphe0_IN_1_GW_4.jpg
+dot -Tjpeg graphe0_IN_2_GW_1.dot  -o graphe0_IN_2_GW_1.jpg
+dot -Tjpeg graphe0_IN_2_GW_2.dot  -o graphe0_IN_2_GW_2.jpg
+dot -Tjpeg graphe0_IN_2_GW_3.dot  -o graphe0_IN_2_GW_3.jpg
+dot -Tjpeg graphe0_IN_2_GW_4.dot  -o graphe0_IN_2_GW_4.jpg
+dot -Tjpeg graphe0_IN_3_GW_1.dot  -o graphe0_IN_3_GW_1.jpg
+dot -Tjpeg graphe0_IN_3_GW_2.dot  -o graphe0_IN_3_GW_2.jpg
+dot -Tjpeg graphe0_IN_3_GW_3.dot  -o graphe0_IN_3_GW_3.jpg
+dot -Tjpeg graphe0_IN_3_GW_4.dot  -o graphe0_IN_3_GW_4.jpg
+dot -Tjpeg graphe0_IN_4_GW_1.dot  -o graphe0_IN_4_GW_1.jpg
+dot -Tjpeg graphe0_IN_4_GW_2.dot  -o graphe0_IN_4_GW_2.jpg
+dot -Tjpeg graphe0_IN_4_GW_3.dot  -o graphe0_IN_4_GW_3.jpg
+dot -Tjpeg graphe0_IN_4_GW_4.dot  -o graphe0_IN_4_GW_4.jpg
